da56afd0f3f8a855b25ea58dbd2350bc3f2b1669
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-09-07 17:05:44 +0200

    pipeline parameters

---
+++ json_to_spreadsheet/pipeline_parameters_mapatext.ps1
@@ -0,0 +1,9 @@
+$input_dir = "..\mapatext"
+$input_flows_name = "PH-plh-international-flavour_expire_ABtesting_fil.json" 
+$mapping_file_name = "plh_mappings.json"
+$deployment = "Philippines"
+$house_type = "european"
+
+$lang = "fil"
+
+.\rapidprojson_to_excel_pipeline.ps1
---
+++ json_to_spreadsheet/pipeline_parameters_templates.ps1
@@ -0,0 +1,8 @@
+$input_dir = "..\templates_for_blocks"
+$input_flows_name = "test-excel-rapidpro-conversion.json" 
+$mapping_file_name = "block_mappings.json"
+$deployment = "Global"
+$house_type = "european"
+
+
+.\rapidprojson_to_excel_pipeline.ps1
